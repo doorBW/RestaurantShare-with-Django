fea1774f6a47fb9d713362628ea666e70cda0629
doorBW <doorbw@outlook.com>
2019-07-08 21:53:38 +0900

    url&templates setting finish

--- RestaurantShare/shareRes/urls.py
+++ RestaurantShare/shareRes/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('/', views.index),
+    path('', views.index),
     path('restaurantDetail/',views.restaurantDetail),
     path('restaurantCreate/',views.restaurantCreate),
     path('categoryCreate/',views.categoryCreate)
--- RestaurantShare/shareRes/views.py
+++ RestaurantShare/shareRes/views.py
@@ -3,13 +3,17 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("index")
+    # return HttpResponse("index")
+    return render(request, 'shareRes/index.html')
     
 def restaurantDetail(request):
-    return HttpResponse("restaurantDetail")
+    # return HttpResponse("restaurantDetail")
+    return render(request, 'shareRes/restaurantDetail.html')
     
 def restaurantCreate(request):
-    return HttpResponse("restaurantCreate")
+    # return HttpResponse("restaurantCreate")
+    return render(request, 'shareRes/restaurantCreate.html')
     
 def categoryCreate(request):
-    return HttpResponse("categoryCreate")
+    # return HttpResponse("categoryCreate")
+    return render(request, 'shareRes/categoryCreate.html')
