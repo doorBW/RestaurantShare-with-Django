890830907513eaf94d3deb88b158bccce2644fbf
doorBW <doorbw@outlook.com>
2019-07-09 23:33:36 +0900

    category Create&Read finish

--- RestaurantShare/shareRes/urls.py
+++ RestaurantShare/shareRes/urls.py
@@ -3,8 +3,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('restaurantDetail/',views.restaurantDetail),
-    path('restaurantCreate/',views.restaurantCreate),
-    path('categoryCreate/',views.categoryCreate)
+    path('', views.index, name='index'),
+    path('restaurantDetail/',views.restaurantDetail, name='resDetailPage'),
+    path('restaurantCreate/',views.restaurantCreate, name='resCreatePage'),
+    path('categoryCreate/',views.categoryCreate, name='cateCreatePage'),
+    path('categoryCreate/create',views.Create_category, name='cateCreate')
 ]
--- RestaurantShare/shareRes/views.py
+++ RestaurantShare/shareRes/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import *
 
 # Create your views here.
 def index(request):
+    categories = Category.objects.all()
+    content = {'categories': categories}
     # return HttpResponse("index")
-    return render(request, 'shareRes/index.html')
+    return render(request, 'shareRes/index.html', content)
     
 def restaurantDetail(request):
     # return HttpResponse("restaurantDetail")
@@ -15,5 +19,14 @@ def restaurantCreate(request):
     return render(request, 'shareRes/restaurantCreate.html')
     
 def categoryCreate(request):
+    categories = Category.objects.all()
+    content = {'categories': categories}
     # return HttpResponse("categoryCreate")
-    return render(request, 'shareRes/categoryCreate.html')
+    return render(request, 'shareRes/categoryCreate.html', content)
+
+def Create_category(request):
+    category_name = request.POST['categoryName']
+    new_category = Category(category_name = category_name)
+    new_category.save()
+    return HttpResponseRedirect(reverse('index'))
+    # return HttpResponse("여기서 category Create 기능을 구현할거야.")
