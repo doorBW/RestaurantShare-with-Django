2feca4ed1f1a0d77d16032d77788c39773ef8d53
doorBW <doorbw@outlook.com>
2019-07-12 00:44:51 +0900

    res Update finish

--- RestaurantShare/shareRes/urls.py
+++ RestaurantShare/shareRes/urls.py
@@ -1,10 +1,12 @@
-# sendEmail > urls.py
+# shareRes > urls.py
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('restaurantDetail/<str:res_id>',views.restaurantDetail, name='resDetailPage'),
+    path('restaurantDetail/updatePage/update',views.Update_restaurant, name='resUpdate'),
+    path('restaurantDetail/updatePage/<str:res_id>',views.restaurantUpdate, name='resUpdatePage'),
     path('restaurantCreate/',views.restaurantCreate, name='resCreatePage'),
     path('restaurantCreate/create',views.Create_restaurant, name='resCreate'),
     path('categoryCreate/',views.categoryCreate, name='cateCreatePage'),
--- RestaurantShare/shareRes/views.py
+++ RestaurantShare/shareRes/views.py
@@ -23,6 +23,29 @@ def restaurantCreate(request):
     # return HttpResponse("restaurantCreate")
     return render(request, 'shareRes/restaurantCreate.html', content)
 
+def restaurantUpdate(request,res_id):
+    categories = Category.objects.all()
+    restaurant = Restaurant.objects.get(id = res_id)
+    content = {'categories': categories, 'restaurant': restaurant}
+    return render(request, 'shareRes/restaurantUpdate.html', content)
+
+def Update_restaurant(request):
+    resId = request.POST['resId']
+    change_category_id = request.POST['resCategory']
+    change_category = Category.objects.get(id = change_category_id)
+    change_name = request.POST['resTitle']
+    change_link = request.POST['resLink']
+    change_content = request.POST['resContent']
+    change_keyword = request.POST['resLoc']
+    before_restaurant = Restaurant.objects.get(id = resId)
+    before_restaurant.category = change_category
+    before_restaurant.restaurant_name = change_name
+    before_restaurant.restaurant_link = change_link
+    before_restaurant.restaurant_content = change_content
+    before_restaurant.restaurant_keyword = change_keyword
+    before_restaurant.save()
+    return HttpResponseRedirect(reverse('resDetailPage', kwargs={'res_id':resId}))
+
 def Create_restaurant(request):
     category_id = request.POST['resCategory']
     category = Category.objects.get(id = category_id)
