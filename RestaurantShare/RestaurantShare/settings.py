df5023f236ab6b12f82e0eff1f8a04651cc65bc5
doorBW <doorbw@outlook.com>
2019-08-10 22:43:08 +0900

    setting

--- RestaurantShare/RestaurantShare/settings.py
+++ RestaurantShare/RestaurantShare/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ju(o4)fxsjko4^z)psjg*uq5=-+crow6-x(b=xdk7s0tp5$5@m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["djangopracticeaccount.pythonanywhere.com"]
+ALLOWED_HOSTS = ["djangopracticeaccount.pythonanywhere.com","127.0.0.1","localhost"]
 
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
